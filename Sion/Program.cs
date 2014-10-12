d240d398633973d2855fb73f6f36492a33246261
jonibahu <joniwalensi@googlemail.com>
2014-10-12 16:53:44 +0200

    Update Program.cs

--- Sion/Program.cs
+++ Sion/Program.cs
@@ -88,8 +88,7 @@ namespace Sion
 
         private void OnDraw(EventArgs args)
         {
-            if (Player.IsDead) return;
-            if (Config.Item("DrawQ").GetValue<bool>() && SkillQ.Level > 0) Utility.DrawCircle(Player.Position, Q.Range, System.Drawing.Color.White);
+            if (Config.Item("DrawQ").GetValue<bool>() && SkillQ.Level > 0) Utility.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.White);
         }
 
         static void Game_OnGameProcessPacket(GamePacketEventArgs args)
