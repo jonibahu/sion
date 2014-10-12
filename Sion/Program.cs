3bb6f06e95eda13dfb81f2ad8ad1628de380dfb7
jonibahu <joniwalensi@googlemail.com>
2014-10-12 15:24:33 +0200

    Update Program.cs

--- Sion/Program.cs
+++ Sion/Program.cs
@@ -88,7 +88,7 @@ namespace Sion
 
         static void Drawing_OnDraw(EventArgs args)
         {
-        if (Config.Item("DrawQ") > 0) Utility.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.White);
+        if (Config.Item("DrawQ") == false) Utility.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.White);
         }
 
         static void Game_OnGameProcessPacket(GamePacketEventArgs args)
